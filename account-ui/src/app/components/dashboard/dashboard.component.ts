b975acc39194b05ba54a8e988011b63c73aed946
Kamil <kamil.noganski@gmail.com>
2016-10-27 20:02:45 +0200

    Fill user template with data.

--- account-ui/src/app/components/dashboard/dashboard.component.ts
+++ account-ui/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {Http} from "@angular/http";
 
 @Component({
     selector: 'my-dashboard',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-    ngOnInit() {
+    constructor(private http: Http) {
+        this.userData = {};
     }
 
+    ngOnInit() {
+        this.http.get('/account/api/user')
+            .map(res => res.json())
+            .subscribe(
+                data => this.userData = data,
+                err => this.logError(err)
+            );
+    }
+
+    logError(err) {
+        console.error('There was an error: ' + err);
+    }
 }
