3792f1abbeae9e810153541e0e371ddea5ce1b3d
Kamil <kamil.noganski@gmail.com>
2016-11-02 19:55:27 +0100

    Password form validation.

---
+++ account-ui/src/app/components/profile/change.password.form.ts
@@ -0,0 +1,7 @@
+
+export class ChangePasswordForm {
+    constructor(
+        public password: string,
+        public confirmPassword: string,
+    ) {  }
+}
--- account-ui/src/app/components/profile/change.password.ts
+++ account-ui/src/app/components/profile/change.password.ts
@@ -1,31 +1,29 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Http} from "@angular/http";
 import {Router} from '@angular/router'
+import {ChangePasswordForm} from './change.password.form'
 
 @Component({
     selector: 'user-change-password',
     templateUrl: 'change.password.html'
 })
-export class ChangePasswordComponent {
+export class ChangePasswordComponent implements OnInit{
+
 
     constructor(private http: Http, private router: Router) {
-        this.form = {
-            password: "",
-            confirmPassword: ""
-        };
+    }
+
+    ngOnInit() {
+        this.form = new ChangePasswordForm('','');
     }
 
     save($event) {
         $event.preventDefault();
-        if (!this.form .password || !this.form .confirmPassword) {
-            console.warn("Password is required");
-            return;
-        }
+
         if (this.form.password !== this.form .confirmPassword) {
             console.warn("Passwords do not match");
             return;
         }
-        console.log(this.form);
         this.http.post('/account/api/user/password', this.form )
             .map(res => res.json())
             .subscribe(
