ba585e496a14d216d31b678c3846c35b2f3012a8
Kamil <kamil.noganski@gmail.com>
2016-11-02 20:14:52 +0100

    Password form validation - validations for equal passwords.

--- account-ui/src/app/components/profile/change.password.ts
+++ account-ui/src/app/components/profile/change.password.ts
@@ -1,35 +1,42 @@
 import {Component, OnInit} from "@angular/core";
 import {Http} from "@angular/http";
-import {Router} from '@angular/router'
-import {ChangePasswordForm} from './change.password.form'
+import {Router} from "@angular/router";
+import {ChangePasswordForm} from "./change.password.form";
 
 @Component({
     selector: 'user-change-password',
     templateUrl: 'change.password.html'
 })
-export class ChangePasswordComponent implements OnInit{
+export class ChangePasswordComponent implements OnInit {
 
 
     constructor(private http: Http, private router: Router) {
     }
 
     ngOnInit() {
-        this.form = new ChangePasswordForm('','');
+        this.form = new ChangePasswordForm('', '');
     }
 
     save($event) {
         $event.preventDefault();
 
-        if (this.form.password !== this.form .confirmPassword) {
+        if (this.form.password !== this.form.confirmPassword) {
             console.warn("Passwords do not match");
             return;
         }
-        this.http.post('/account/api/user/password', this.form )
+        this.http.post('/account/api/user/password', this.form)
             .map(res => res.json())
             .subscribe(
                 data => this.onPasswordChange(data),
                 err => this.logError(err)
             );
+    }
+
+    arePasswordsEqual(password, confirmPassword) {
+        if (!password.valid || !confirmPassword.valid) {
+            return true;
+        }
+        return password.model === confirmPassword.model;
     }
 
     onPasswordChange(data) {
